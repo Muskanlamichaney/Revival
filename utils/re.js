0497194d4f83a406e9879caf59ec742004a6a37a
Muskanlamichaney <sristylamichaney@gmail.com>
2024-07-06 09:57:26 +0530

    Delete utils directory

--- utils/re.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("fb-btn").addEventListener("click", function() {
-        window.open("https://www.facebook.com", "_blank");
-    });
-
-    document.getElementById("ig-btn").addEventListener("click", function() {
-        window.open("https://www.instagram.com", "_blank");
-    });
-});
