5050d26ae84550defb3c082afbc8e5e428dd0c97
Muskanlamichaney <sristylamichaney@gmail.com>
2024-07-06 09:58:25 +0530

    Add files via upload

---
+++ utils/re.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("fb-btn").addEventListener("click", function() {
+        window.open("https://www.facebook.com", "_blank");
+    });
+
+    document.getElementById("ig-btn").addEventListener("click", function() {
+        window.open("https://www.instagram.com", "_blank");
+    });
+});
